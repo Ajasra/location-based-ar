e943a3813c98e3576b9ba940a8ca31263acfaa79
Vasily.onl <betinvasilij@gmail.com>
2021-05-25 22:34:41 +0800

    u

--- peakfinder.js
+++ peakfinder.js
@@ -1,5 +1,7 @@
 AFRAME.registerComponent('peakfinder', {
     init: function() {
         alert('Peakfinder component initialising!');
+        const distanceMsg = document.querySelector('[gps-projected-entity-place]').getAttribute('distanceMsg');
+        alert(distanceMsg);   // "890 meters"
     }
 });
